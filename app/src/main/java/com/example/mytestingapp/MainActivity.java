acd3b97628198e3545bd3ba4ed9578e1a72a2a79
Dodo Henschel <54441501+dodo-nainggolan@users.noreply.github.com>
2019-10-19 20:10:13 +0700

    Completed

--- app/src/main/java/com/example/mytestingapp/MainActivity.java
+++ app/src/main/java/com/example/mytestingapp/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.mytestingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.core.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnSetValue;
+    private TextView tvText;
+    private ArrayList<String> names;
+    ImageView imgPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSetValue = findViewById(R.id.btn_set_value);
+        tvText = findViewById(R.id.tv_text);
+        btnSetValue.setOnClickListener(this);
+
+        names = new ArrayList<>();
+        names.add("Narenda Wicaksono");
+        names.add("Kevin");
+        names.add("Yoza");
+
+        imgPreview = findViewById(R.id.img_preview);
+        //imgPreview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fronalpstock_big));
+        Glide.with(this).load(R.drawable.fronalpstock_big).into(imgPreview);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_set_value) {
+            StringBuilder name = new StringBuilder();
+            for (int i = 0; i < 3; i++) {
+                name.append(names.get(i)).append("\n");
+            }
+
+            tvText.setText(name.toString());
+        }
+
     }
 }
